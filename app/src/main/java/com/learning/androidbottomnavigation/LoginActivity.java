e8f984462b876d1d444f66cd50951e5bf5cc37df
Krishna <mathi3567>
2020-12-08 12:23:38 +0530

    Video Call Login Updated

---
+++ app/src/main/java/com/learning/androidbottomnavigation/LoginActivity.java
@@ -0,0 +1,29 @@
+package com.learning.androidbottomnavigation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private Button signIn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        signIn = findViewById(R.id.buttonSignIn);
+
+        signIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
