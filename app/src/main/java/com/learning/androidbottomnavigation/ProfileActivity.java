dd685f25bd1f28082e6da4c183e4dff0626e4be6
Krishna <mathi3567>
2020-12-12 21:18:10 +0530

    Video Call Login Updated

--- app/src/main/java/com/learning/androidbottomnavigation/ProfileActivity.java
+++ app/src/main/java/com/learning/androidbottomnavigation/ProfileActivity.java
@@ -3,12 +3,27 @@ package com.learning.androidbottomnavigation;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class ProfileActivity extends AppCompatActivity {
+
+    LinearLayout profileEmailEdit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        profileEmailEdit = findViewById(R.id.profileEmailEdit);
+
+        profileEmailEdit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
