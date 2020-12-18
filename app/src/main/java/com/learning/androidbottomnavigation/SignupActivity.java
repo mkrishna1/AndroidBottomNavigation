c3bf5c15e92af2585c3d401d8c11284cdc79d3e8
Krishna <mathi3567>
2020-12-18 21:53:46 +0530

    Video Call Signup Updated

---
+++ app/src/main/java/com/learning/androidbottomnavigation/SignupActivity.java
@@ -0,0 +1,20 @@
+package com.learning.androidbottomnavigation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+public class SignupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+
+    }
+}
